3e2dd7cfb34454c241af8a6377c09ffe9f333d7e
Aramdhann <radityamochamad@gmail.com>
2024-04-02 14:19:49 +0700

    update style component

--- resources/views/components/style.blade.php
+++ resources/views/components/style.blade.php
@@ -1,1 +1,6 @@
+<link rel="shortcut icon" href="{{ url('assets/images/logo.png')}}" type="image/x-icon">
+
 <link rel="stylesheet" href="{{ url('assets/vendors/boxicons/css/boxicons.min.css') }}">
+
+<!-- Scripts -->
+@vite(['resources/sass/app.scss', 'resources/js/app.js'])
