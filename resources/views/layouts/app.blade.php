4d1a2b1fb88daf66510c2ed2015f379dbef4c7e8
Aramdhann <radityamochamad@gmail.com>
2024-04-02 14:20:01 +0700

    delete navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,83 +8,10 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     @include('components.style')
-
-    <!-- Scripts -->
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md shadow-sm bg-body-tertiary py-3">
-            <div class="container">
-                <a class="navbar-brand logo" href="{{ url('/') }}">
-                    <img src="{{ url('assets/images/logo.png') }}" alt="logo kasir online"> Kasir Online
-                </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mx-auto gap-2 mt-3 mt-md-0">
-                        <li class="nav-item">
-                            <a href="dashboard.html" class="nav-link active">
-                                <i class="bx bxs-dashboard"></i> Dashboard
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="dashboard-menu.html" class="nav-link">
-                                <i class="bx bx-food-menu"></i> Menu
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="dashboard-pendapatan.html" class="nav-link">
-                                <i class="bx bx-money"></i> Pendapatan
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="dashboard-kasir.html" class="nav-link">
-                                <i class="bx bx-user-pin"></i> Kasir
-                            </a>
-                        </li>
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ms-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                        @if (Route::has('login'))
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                        </li>
-                        @endif
-
-                        @if (Route::has('register'))
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                        </li>
-                        @endif
-                        @else
-                        <li class="nav-item dropdown">
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                {{ Auth::user()->name }}
-                            </a>
-
-                            <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                    {{ __('Logout') }}
-                                </a>
-
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                    @csrf
-                                </form>
-                            </div>
-                        </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
+        @include('components.navbar-item')
 
         <main class="py-4">
             @yield('content')
