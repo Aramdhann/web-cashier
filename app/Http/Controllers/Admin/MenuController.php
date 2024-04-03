b91c967d2b88a36905facb530a6e4850f740abfd
Aramdhann <radityamochamad@gmail.com>
2024-04-03 13:17:34 +0700

    create menu controller for crud

---
+++ app/Http/Controllers/Admin/MenuController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Menu;
+use Illuminate\Http\Request;
+
+class MenuController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return view('pages.admin.menus.index', [
+            'title' => 'Semua Menu',
+            'menus' => Menu::orderBy('id', 'DESC')->get()
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('pages.admin.menus.create', [
+            'title' => 'Buat Menu Baru',
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $data = $request->all();
+        $data['image'] = $request->file('image')->store('menus', 'public');
+        Menu::create($data);
+
+        return redirect()->route('menu.index')->with('Success', 'Menu berhasil ditambahkan.');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
